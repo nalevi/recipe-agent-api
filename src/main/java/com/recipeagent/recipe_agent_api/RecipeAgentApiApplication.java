5048434635942d6fbd9073b2413e8b9b3c6407ce
Nagy Levente <woundless17@gmail.com>
2024-07-21 18:49:07 +0200

    Finished 1.0.0 of the API spec
    Also, added the initial spring app skeleton.

---
+++ src/main/java/com/recipeagent/recipe_agent_api/RecipeAgentApiApplication.java
@@ -0,0 +1,13 @@
+package com.recipeagent.recipe_agent_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeAgentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeAgentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recipeagent/recipe_agent_api/RecipeAgentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipeagent.recipe_agent_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAgentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
